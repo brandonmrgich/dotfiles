4fb02e2c0ce4a53b199a7f0832fb30597b09ee0d
brandonmrgich <58794109+brandonmrgich@users.noreply.github.com>
2026-04-24 18:24:26 -0400

    Updated git config, added work alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,9 +1,15 @@
 # aliases.zsh — shell aliases
 
+# Defaults
 alias vim='nvim'
+
+# Shorthands
 alias chvim='vim ~/.config/nvim/'
 alias venv='. $(pwd)/venv/bin/activate'
 alias psef='ps -ef | grep -v grep | grep'
 alias c='clear'
 alias todo='grep -Ri "todo:" --exclude-dir={node_modules,.git,build}'
 alias loc='tokei'
+
+# Navigation
+alias work='cd ~/Development/GitHubProjects/MusicPortfolio/'
